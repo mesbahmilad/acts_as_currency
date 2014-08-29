92ac54e11f01851fbcec572156851690aa715edb
Milad Mesbah <mesbahmilad@gmail.com>
2014-08-28 18:37:45 -0700

    modifying location of method override

--- lib/acts_as_currency.rb
+++ lib/acts_as_currency.rb
@@ -10,18 +10,18 @@ module ActsAsCurrency
       configuration[:fields] = attr_names.flatten.uniq.compact
       class_attribute :currency_options
       self.currency_options = configuration
+      self.currency_options[:fields].each do |field|
+        define_method(field, proc{return read_attribute(field).nil? ? 0 : read_attribute(field)/100.0})
+        define_method("#{field}=") do |value|
+          write_attribute(field, (value.to_d*100).to_i)
+        end
+      end
       include ActsAsCurrency::LocalInstanceMethods
     end
   end
 
   module LocalInstanceMethods
     def initialize(attributes = {})
-      self.currency_options[:fields].each do |field|
-        self.class.send(:define_method, field, proc{return read_attribute(field).nil? ? 0 : read_attribute(field)/100.0})
-        self.class.send :define_method, "#{field}=" do |value|
-          write_attribute(field, (value.to_d*100).to_i)
-        end
-      end
       super
     end
   end
